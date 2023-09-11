32d99983413e2054ed1cb467a84c1331f6c97288
Haliunaa <km632@duke.edu>
2023-09-10 20:27:34 -0400

    create testcases

--- main.py
+++ main.py
@@ -6,35 +6,40 @@ def find_mean(df):
     mean = round(df['Loan Amount(in USD)'].mean())
     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     formatted_mean = locale.currency(mean, grouping=True)
-    print("Mean:", formatted_mean)
+    return formatted_mean
 
 def find_median(df):
     median = round(df['Loan Amount(in USD)'].median())
     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     formatted_median = locale.currency(median, grouping=True)
-    print("Median:", formatted_median)
+    return formatted_median
 
 def find_std(df):
     std = round(df['Loan Amount(in USD)'].std())
     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     formatted_std = locale.currency(std, grouping=True)
-    print("Standard deviation:", formatted_std)
+    return formatted_std
 
 def create_graph(df):
     x = df['School Country']
     y = df['Loan Amount(in USD)']
     plt.plot(x,y)
     plt.savefig("plot.png")
+    
 
 
 def main():
     df = pd.read_excel('raw.xlsx')
     new_column_names = {'Эцэг /эх/-ийн нэр / Өөрийн нэр': 'First and Last Name', 'Суралцаж байгаа улс': 'School Country', 'Сургуулийн нэр': 'School Name', 'Мэргэжил':'Intended Major', 'Суралцах хугацаа': 'Study Duration(in years)', 'Олгосон санхүүжил':'Loan Amount(in USD)'}
     df.rename(columns=new_column_names, inplace=True)
-    find_mean(df)
-    find_median(df)
-    find_std(df)
+    mean = find_mean(df)
+    median = find_median(df)
+    std = find_std(df)
     create_graph(df)
+    print("Mean:", mean)
+    print("Median:", median)
+    print("Standard deviation:", std)
+
 
     
 
--- test_1.py
+++ test_1.py
@@ -1,16 +1,26 @@
 import unittest
-from main import add
+from main import find_mean,find_median, find_std
+import pandas as pd
+import locale 
 
+data = {'Loan Amount(in USD)': [1000, 2000, 3000, 4000, 5000]}
+df = pd.DataFrame(data)
 
 class TestSumFunction(unittest.TestCase):
-    def test_sum_positive_numbers(self):
-        self.assertEqual(add(1, 2), 3)
-
-    def test_sum_negative_numbers(self):
-        self.assertEqual(add(-1, -2), -3)
-
-    def test_sum_mixed_numbers(self):
-        self.assertEqual(add(1, -2), -1)
+    def test_stats(self):
+        data = {'Loan Amount(in USD)': [1000, 2000, 3000, 4000, 5000]}
+        df = pd.DataFrame(data)
+        expected_mean = '$3,000.00'
+        expected_median = '$3,000.00'
+        expected_std =  '$1,581.00'
+        mean_result = find_mean(df)
+        median_result = find_median(df)
+        std_result = find_std(df)
+        self.assertEqual(mean_result, expected_mean)
+        self.assertEqual(median_result, expected_median)
+        self.assertEqual(std_result, expected_std)
+        
+        
 
 
 if __name__ == "__main__":
